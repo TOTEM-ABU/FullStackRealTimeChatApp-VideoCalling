33d882e2bee144bb93fb0f409235fb9bd88fa7d8
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-10-18 12:02:16 +0300

    Frontend setup done

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as HomePage } from "./HomePage";
+export { default as SignUpPage } from "./SignUpPage";
+export { default as LoginPage } from "./LoginPage";
+export { default as CallPage } from "./CallPage";
+export { default as ChatPage } from "./ChatPage";
+export { default as NotificationsPage } from "./NotificationsPage";
+export { default as OnboardingPage } from "./OnboardingPage";
