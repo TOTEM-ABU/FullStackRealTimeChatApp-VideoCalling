7e2b3fe13043f7fef73594cdc7dd516cfdb627eb
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-10-18 15:34:46 +0300

    React tanstack query and axios setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.routes.js";
@@ -12,6 +13,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
