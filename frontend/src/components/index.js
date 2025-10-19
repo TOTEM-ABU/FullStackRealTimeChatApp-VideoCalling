4a4491cac918df6ba77fa6b46079b2a523cf8218
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-10-19 21:58:42 +0300

    HomePage done

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as PageLoader } from "./PageLoader.jsx";
 export { default as Layout } from "./Layout.jsx";
+export { default as FriendCard } from "./FriendCard.jsx";
+export { default as NoFriendsFound } from "./NoFriendsFound.jsx";
