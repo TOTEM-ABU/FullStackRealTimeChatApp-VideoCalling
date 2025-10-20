091b44775d3622aad91fb1988823219d4ff39363
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-10-20 21:09:36 +0300

    Chatpage done

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,3 +3,5 @@ export { default as Layout } from "./Layout.jsx";
 export { default as FriendCard } from "./FriendCard.jsx";
 export { default as NoFriendsFound } from "./NoFriendsFound.jsx";
 export { default as NoNotificationsFound } from "./NoNotificationsFound.jsx";
+export { default as ChatLoader } from "./ChatLoader.jsx";
+export { default as CallButton } from "./CallButton.jsx";
